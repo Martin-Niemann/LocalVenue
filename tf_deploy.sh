ef80463c7ea7e455ab24e2b544db950c8e04c6b9
Martin Niemann Madsen <95416410+Martin-Niemann@users.noreply.github.com>
2025-01-16 14:15:58 +0100

    Fix Release zip containing subfolders, preventing deploy.

--- tf_deploy.sh
+++ tf_deploy.sh
@@ -14,7 +14,9 @@ dotnet publish -c Release
 OS=$(uname -s)
 
 if [ "$OS" = "Linux" ] || [ $OS = "Darwin" ]; then # Linux or MacOS
-    zip -r ./LocalVenue/bin/Release/publish.zip ./LocalVenue/bin/Release/net8.0/publish/* 
+    cd ./LocalVenue/bin/Release/net8.0/publish/
+    zip -r ../publish.zip ./*
+    cd -
 else # Windows
     powershell 'Compress-Archive -Path ./LocalVenue/bin/Release/net8.0/publish/* -DestinationPath ./LocalVenue/bin/Release/publish.zip -Force'
 fi
@@ -25,7 +27,7 @@ terraform apply --parallelism=25 -auto-approve
 # Step 5 (optional): Deploy the app, if Terraform failed or is slow to redeploy once the resources are created
 #> az webapp deploy --resource-group localvenue-<suffix> --name localvenue-webapp-<suffix> --src-path "./LocalVenue/bin/Release/publish.zip"
 
-rm ./LocalVenue/bin/Release/publish.zip # Clean up the zip file
+# rm ./LocalVenue/bin/Release/publish.zip # Clean up the zip file
 
 # Step 6: Cleanup can be done with 
 #> terraform destroy -auto-approve
